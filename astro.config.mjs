00a5ef8d0d9f6cdbf1aa0410afdc3f867cf4434e
RavenSam <maksam7dev@gmail.com>
2024-03-25 06:40:54 +0100

    config for netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap(), tailwind(), svelte()],
-})
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: "server",
+  adapter: netlify()
+});
