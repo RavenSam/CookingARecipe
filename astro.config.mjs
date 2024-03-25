069de487ca47de9f9d4cbd6e4cefe00e46e8bfe9
RavenSam <maksam7dev@gmail.com>
2024-03-25 06:53:54 +0100

    another fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
-  output: "server",
-  adapter: netlify()
-});
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), tailwind(), svelte()],
+	output: "hybrid",
+	adapter: netlify(),
+})
