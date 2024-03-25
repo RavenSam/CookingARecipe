3840d35c4a76092e6791bf2fdc5ae2f9fa8816f3
RavenSam <maksam7dev@gmail.com>
2024-03-25 10:52:02 +0100

    site url & getImage test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config"
+
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
-
 import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://cookingarecipee.netlify.app",
+	site: "https://cookingarecipe.com",
 	integrations: [mdx(), sitemap(), tailwind(), svelte()],
 	output: "hybrid",
 	adapter: netlify(),
